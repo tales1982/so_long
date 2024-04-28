c1913a3be91932ccb3b4d3a94fb8c53df1589b65
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-04-28 11:21:44 +0200

    feat: create stack_a and stack_b

--- main.c
+++ main.c
@@ -3,28 +3,28 @@
 
 int	main(int argc, char **argv)
 {
-	int	result;
+	//int	result;
 
-	Stack stack_a, stack_b;
+	Stack a, b;
 	if (argc > 0)
 	{
-		initStack(&stack_a);
-		initStack(&stack_b);
-		push(&stack_a, 20);
-		push(&stack_a, 40);
-		push(&stack_a, 30);
-		push(&stack_a, 10);
-		push(&stack_a, 60);
-		push(&stack_a, 50);
-		result = pop(&stack_a);
+		initStack(&a);
+		initStack(&b);
+		push(&a, 20);
+		push(&a, 40);
+		push(&a, 30);
+		push(&a, 10);
+		push(&a, 60);
+		push(&a, 50);
+		/*result = pop(&a);
 		if (result != -1)
 		{
 			ft_putstr("Valor removido: ");
 			ft_putnbr(result);
 			ft_putstr("\n");
-		}
+		}*/
 		ft_putstr("\n");
-		peek(&stack_a);
+		peek(&a);
 		ft_putstr(argv[0]);
 		return (0);
 	}
